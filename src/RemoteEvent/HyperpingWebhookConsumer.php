858127e816db8c07862a6f9be55241e4912bc130
Floran Brutel <fbrutel@akollade.fr>
2025-03-27 11:35:32 +0100

    Setup first working version

---
+++ src/RemoteEvent/HyperpingWebhookConsumer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\RemoteEvent;
+
+use Symfony\Component\Notifier\Message\PushMessage;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\TexterInterface;
+use Symfony\Component\RemoteEvent\Attribute\AsRemoteEventConsumer;
+use Symfony\Component\RemoteEvent\Consumer\ConsumerInterface;
+use Symfony\Component\RemoteEvent\RemoteEvent;
+
+#[AsRemoteEventConsumer('hyperping')]
+final class HyperpingWebhookConsumer implements ConsumerInterface
+{
+    public function __construct(private readonly TexterInterface $texter)
+    {
+    }
+
+    public function consume(RemoteEvent $event): void
+    {
+        $payload = $event->getPayload();
+
+        $notification = (new PushMessage('Monitoring', $payload['text']));
+
+        $this->texter->send($notification);
+    }
+}
